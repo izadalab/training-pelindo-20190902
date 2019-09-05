fd981ac308a5b533bf7d4f0dcc4d8eb3eb30f971
Fordyta Abubakar <dhytodev@gmail.com>
2019-09-05 17:20:55 +0800

    show movies to recyclerview

--- MovieList/app/src/main/java/id/co/inaportempat/movielist/ui/popular/PopularMoviesFragment.kt
+++ MovieList/app/src/main/java/id/co/inaportempat/movielist/ui/popular/PopularMoviesFragment.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import id.co.inaportempat.movielist.R
+import id.co.inaportempat.movielist.model.Movie
+import id.co.inaportempat.movielist.ui.adapter.MovieRvAdapter
+import kotlinx.android.synthetic.main.fragment_popular_movies.*
 
 class PopularMoviesFragment : Fragment() {
+
+    private lateinit var movieRvAdapter: MovieRvAdapter
+    private lateinit var movieList: List<Movie>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,4 +24,28 @@ class PopularMoviesFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_popular_movies, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView() {
+        movieList = generateMovies()
+        movieRvAdapter = MovieRvAdapter(movieList)
+        rvMovies.apply {
+//            layoutManager = LinearLayoutManager(requireActivity())
+            layoutManager = GridLayoutManager(requireActivity(), 2)
+            setHasFixedSize(true)
+            adapter = movieRvAdapter
+        }
+    }
+
+    private fun generateMovies(): List<Movie> = listOf(
+        Movie("A Star", R.drawable.poster_a_star, ""),
+        Movie("Aquaman", R.drawable.poster_aquaman, ""),
+        Movie("Avenger Infinity", R.drawable.poster_avengerinfinity, ""),
+        Movie("Deadpool", R.drawable.poster_deadpool, ""),
+        Movie("Dragon Ball", R.drawable.poster_dragonball, "")
+    )
 }
