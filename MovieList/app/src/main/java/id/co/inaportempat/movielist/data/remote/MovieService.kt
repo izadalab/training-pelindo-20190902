723ad919042190dd250744233538775f12b5e6a0
Fordyta Abubakar <dhytodev@gmail.com>
2019-09-07 09:42:13 +0800

    add logging interceptor

--- MovieList/app/src/main/java/id/co/inaportempat/movielist/data/remote/MovieService.kt
+++ MovieList/app/src/main/java/id/co/inaportempat/movielist/data/remote/MovieService.kt
@@ -1,7 +1,11 @@
 package id.co.inaportempat.movielist.data.remote
 
+import id.co.inaportempat.movielist.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object MovieService {
     private const val BASE_URL = "https://api.themoviedb.org/3/"
@@ -9,9 +13,24 @@ object MovieService {
     fun makeService(): MovieApiEndpoint {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(makeOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         return retrofit.create(MovieApiEndpoint::class.java)
+    }
+
+    private fun makeOkHttpClient() : OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY
+        else
+            HttpLoggingInterceptor.Level.NONE
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
     }
 }
