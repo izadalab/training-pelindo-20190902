665796b1989adc69007d800b41e13a62397f3f3d
Fordyta Abubakar <dhytodev@gmail.com>
2019-09-08 13:46:50 +0800

    add trailer model and getTrailersByMovieId endpoint function

--- MovieList/app/src/main/java/id/co/inaportempat/movielist/data/remote/MovieApiEndpoint.kt
+++ MovieList/app/src/main/java/id/co/inaportempat/movielist/data/remote/MovieApiEndpoint.kt
@@ -1,18 +1,27 @@
 package id.co.inaportempat.movielist.data.remote
 
 import id.co.inaportempat.movielist.model.MovieResponse
+import id.co.inaportempat.movielist.model.TrailerResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApiEndpoint {
     @GET("movie/now_playing")
     fun getNowPlayingMovies(
-        @Query("api_key") apiKey:String
+        @Query("api_key") apiKey: String
     ): Call<MovieResponse>
 
     @GET("movie/popular")
     fun getPopularMovies(
-        @Query("api_key") apiKey:String
+        @Query("api_key") apiKey: String
     ): Call<MovieResponse>
+
+    @GET("movie/{movie_id}/videos")
+    fun getTrailersByMovieId(
+        @Query("api_key") apiKey: String,
+        @Path("movie_id") movieId: Int
+    ): Call<TrailerResponse>
 }
